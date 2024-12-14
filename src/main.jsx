13bc9f4034590489013a091cf1e82240821a1122
swanith1234 <swanithpidugu@gmail.com>
2024-12-14 07:58:08 +0530

    Add folder: src

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App userData={{"name":"oyesss","emailId":"pidugubunny534@gmail.com","phoneNo":"4754278457","projects":[{"title":"dklnvdfn","description":"dfmf","technologies":["Java"],"repoLink":"sdklfd","_id":"675cecb8b66fb925b038c792"}],"resume":"","experiences":[{"companyName":"ksdjfdsj","logo":"http://res.cloudinary.com/dgthxi1sd/image/upload/v1734139787/evolumin/cgblf43qfgo7hvqimxag.jpg","role":"dsnjf","description":"sdknfdj","technologies":["React"],"duration":"djsndj","_id":"675cecb8b66fb925b038c793"}],"techStacks":["React"],"contactDetails":["www.github.com"],"codingProfiles":["www.geeksforgeeks.org"],"certifications":["https://media.licdn.com/dms/image/v2/D562DAQG7t3vgDc5z_Q/profile-treasury-image-shrink_1920_1920/profile-treasury-image-shrink_1920_1920/0/1719581374680?e=1734613200&v=beta&t=4tJtnTcR6SUMLSLzcnrIN072ED_Jc2KRzgLDyKY4qt4","https://media.licdn.com/dms/image/v2/D5622AQFyRtKXd4zHiQ/feedshare-shrink_800/feedshare-shrink_800/0/1725456625439?e=1736985600&v=beta&t=hYUOxmmaEqlhmV9Bvc8B3Arx_DB0WEG-nzY7gv4nsxA","https://d8it4huxumps7.cloudfront.net/lambda-pdfs/certificate-images/fdc634be-475e-4fe0-964b-b959d9e0d9c5.jpg"],"achievements":[{"title":"sdkfand","description":"adkfng","_id":"675cecb8b66fb925b038c794"}],"about":"sdf","profilePhoto":"http://res.cloudinary.com/dgthxi1sd/image/upload/v1734139756/evolumin/up0mdfeb97qir82tpeue.webp","_id":"675cecb8b66fb925b038c791","createdAt":"2024-12-14T02:26:00.046Z","updatedAt":"2024-12-14T02:26:00.046Z","__v":0}} />
+  </StrictMode>,
+)
